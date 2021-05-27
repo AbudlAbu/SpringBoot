07d8c8e5ee1f255ba098516933ea1d48499502df
Abu I <84893883+AbudlAbu@users.noreply.github.com>
2021-05-27 15:09:05 +0530

    Add files via upload

---
+++ src/main/java/com/hcl/springcurd/demo/CurdSpringDemoApplication.java
@@ -0,0 +1,13 @@
+package com.hcl.springcurd.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurdSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurdSpringDemoApplication.class, args);
+	}
+
+}
